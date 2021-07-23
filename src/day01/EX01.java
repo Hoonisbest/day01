f429520930fc05bbd179e31d5835470c7a3de0fd
Hoon <dkhj0615@naver.com>
2021-07-23 11:19:15 +0900

    주석방법

--- src/day01/EX01.java
+++ src/day01/EX01.java
@@ -2,8 +2,23 @@ package day01;
 
 public class EX01 {
 	public static void main(String[] args) {
-		System.out.println("안녕하세요");
-		System.out.println("자바 시간입니다");
+	System.out.println("안녕하세요");
+	System.out.println("자바 시간입니다");
+	//println의 경우는 한줄바꿈이 포함된 출력명령어 입니다.
+	//;(세미콜론)의 경우 문장의 끝을 알려줍니다.
+	System.out.print("안녕하세요");
+	System.out.print("자바 시간입니다");
+	
+	//print의 경우는 한줄바꿈이 포함되지 않은 출력 명령어 입니다
+	
+	//의경우는 한줄주석입니다
+
+	/*의 경우는 여러줄
+	 주석입니다*/ 
+	
+	//ctrl shift s는 저장
+	
+	
 	}	
 }
 
