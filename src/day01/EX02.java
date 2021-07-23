4997e945d54e832e40a53cd45b1e818bcefaf6e2
Hoon <dkhj0615@naver.com>
2021-07-23 11:32:58 +0900

    Ex02생성

---
+++ src/day01/EX02.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class EX02 {
+	public static void main(String[] args) {
+		/*
+		\n : 엔터와 비슷하다
+		\t : 탭크기 만큼 오른쪽으로 이동
+		\" : "는 일반 문자로 사용할때
+		\\ : \을 표현할때 사용
+		 */
+		
+	}
+}
